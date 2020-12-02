494d64c546231e06606f5fbaf7e29d9aa8a7236b
Tom Rumore <rumoretom@gmail.com>
2020-12-02 11:06:13 -0500

    changed format and completed adding questions to JS

--- script.js
+++ script.js
@@ -14,10 +14,72 @@ var score = 0;
 
 //questions//
 
-var quizQuestions = [
+const myQuestions = [
   {
-    question:
-  }
+    question: "What steps would you follow to push files from your local machine to your Git Repo?",
+    answers: {
+      a: "Lather, Rinse, Repeat",
+      b: "Git Add, Git Commit, Git Push",
+      c: "Git Clone, Git Pull, Git Stash",
+      d: "Stop, Drop, Roll"
+    },
+    correctAnswer: "b"
+  },
+  {    question: "What does HTML stand for?",
+  answers: {
+    a: "Hypertext Markup Language",
+    b: "Hypertext Media Link",
+    c: "Hide The Meatballs Later",
+    d: "Hyperlink To Media Link"
+  },
+  correctAnswer: "a"
+},
+{     question: "What is CSS used for?",
+  answers: {
+  a: "Nothing, its just some letters",
+  b: "Creating a basic text only page",
+  c: "Stylizing an HTML document",
+  d: "Cleaning System Software"
+},
+  correctAnswer: "c" 
+},
+{     question: "Which of these is not an example of a Bootstrap component?",
+answers: {
+a: "Badges",
+b: "Buttons",
+c: "Mousetraps",
+d: "Cards"
+},
+correctAnswer: "c" 
+},
+{     question: "Facebook is a good example of a webpage with an obvious Grid Layout",
+answers: {
+a: "True",
+b: "False"
+},
+correctAnswer: "a" 
+},
+{     question: "Within a Bootstrap grid, the sum total of all columns within a row must add up to:",
+answers: {
+a: "8",
+b: "10",
+c: "12",
+d: "14"
+},
+correctAnswer: "c" 
+},
+{
+  question: "In Javascript, which of these is not considered one of the nouns of programming?",
+  answers: {
+  a: "Numbers",
+  b: "Space Bar",
+  c: "Booleans",
+  d: "Strings"
+  },
+  correctAnswer: "b" 
+}
+
+
 ]
 
 // timer element//
